e8bc734dd66beaf86e4693227df0c55840a867c1
Kananek Thongkam <info.dvgamer@gmail.com>
2016-01-15 00:20:53 +0700

    site.ath.cx

---
+++ site.ath.cx/body.php
@@ -0,0 +1,1 @@
+<center><h1>Chat Site.</h1><h2>Coming Soon!!!</h2></center>
---
+++ site.ath.cx/configuration.inc.php
@@ -0,0 +1,7 @@
+<?php
+$siteTitle = "life It'Chat Box | Loading...";
+$sitekeywords = "d[v]g, dvg, hakko, kunanek, thongkam, kunanek thongkam";
+$siteDescription = "life It'Chat Box";
+$siteIcon = "favorite.ico";
+$siteLanguage = "language.thai.php"
+?>
